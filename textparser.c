f5c29a25468b47dcc0a261ed0b9c9a23c76261e8
link <ks6423156@gmail.com>
2025-10-23 19:39:07 +0530

    Update textparser.c

--- textparser.c
+++ textparser.c
@@ -13,42 +13,39 @@ void main() {
     int fd_in = 0, fd_out = 0;
     size_t shstrndx = 0, n = 0;
 
-    // Necessary to call other libelf library functions
+   
     elf_version(EV_CURRENT);
 
-    // Open main
+
     fd_in = open("main", O_RDONLY, 0);
 
-    // Convert the file descriptor to an ELF handle
+    // convert the file descriptor to an elf handle
     elf = elf_begin(fd_in, ELF_C_READ, NULL);
 
-    // Retrieve the index of the section name string table
+    // get the index of the string table 
     elf_getshdrstrndx(elf, &shstrndx);
 
-    // Iterate through the sections
+    // go thru the sections
     while ((scn = elf_nextscn(elf, scn)) != NULL) {
-        // Retrieve the section header
+        // get the section header
         shdr = elf64_getshdr(scn);
 
-        // Get the name of the section
+        // get name of the section
         name = elf_strptr(elf, shstrndx, shdr->sh_name);
 
-        // Check if the section is .text
+        // check if the section is .text
         if (strcmp(name, ".text") == 0) {
-            // Open the binary output file
+            // open the output file
             fd_out = open("out_libelf", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 
-            // Read the section and write it to the file
+            // read the section and write it to the file
             while ((data = elf_getdata(scn, data)) != NULL) {
                 write(fd_out, data->d_buf, data->d_size);
             }
 
-            // Close the file descriptor of output file
-            close(fd_out);
+         close(fd_out);
         }
     }
-    // Release the ELF descriptor
     elf_end(elf);
-    // Close main
     close(fd_in);   
 }   
